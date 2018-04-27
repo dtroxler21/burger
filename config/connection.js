03182f8f09e5221f5c2bece2071b25f942b87149
dtroxler21 <dtroxler21@gmail.com>
2018-04-27 12:52:22 -0400

    deleting file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "goyanks1",
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "goyanks1",
+    database: "burger_db"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burger_db;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
