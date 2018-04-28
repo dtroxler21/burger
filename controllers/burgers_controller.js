ccd1654b8cdcb5b1f9a40a85a8bb1b0609a9650b
dtroxler21 <dtroxler21@gmail.com>
2018-04-28 09:03:49 -0400

    final changes to functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burger.updateOne({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
